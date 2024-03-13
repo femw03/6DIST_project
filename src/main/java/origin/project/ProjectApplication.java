ed3897f97f5c0cbee033ccb6fd39754fb05582f3
Gones Anseel <gones.anseel@student.uantwerpen.be>
2024-03-13 16:34:02 +0100

    feat(naming): added naming-database and repository.
    Enables MapRepositories to use CrudRepository with NamingMap.
    Adds NamingRepository which extends CrudRepository.

--- src/main/java/origin/project/ProjectApplication.java
+++ src/main/java/origin/project/ProjectApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.data.map.repository.config.EnableMapRepositories;
+import origin.project.naming.map.NamingMap;
 import origin.project.naming.server.NamingServer;
 
 @SpringBootApplication
 //@EnableEurekaServer
+@EnableMapRepositories(mapType = NamingMap.class)
 public class ProjectApplication {
     @Bean
     public NamingServer namingServer() {
---
+++ src/main/java/origin/project/naming/repository/NamingRepository.java
@@ -0,0 +1,8 @@
+package origin.project.naming.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NamingRepository extends CrudRepository<String, Integer> {
+}
