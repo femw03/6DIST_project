79cd5bd4f56cdbe9a10b6e2e958ebfe873c277ff
Nigel <nigel.dm@telenet.be>
2024-03-13 16:56:42 +0100

    feat(naming): Added Rest methods to namingcontroller.

--- src/main/java/origin/project/naming/controller/NamingServerController.java
+++ src/main/java/origin/project/naming/controller/NamingServerController.java
@@ -1,31 +1,39 @@
 package origin.project.naming.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import origin.project.naming.repository.NamingRepository;
 import origin.project.naming.server.NamingServer;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@RestController
 @RequestMapping("/naming-server")
 public class NamingServerController {
 
-    /*@Autowired
-    private NamingServer namingServer;
+    @Autowired
+    private NamingRepository namingRepository;
 
     @PostMapping("/add-node")
     public void addNode(@RequestParam("ipAddress") String ipAddress) {
-        namingServer.addNode(ipAddress);
+        namingRepository.save(ipAddress);
     }
 
     @PostMapping("/remove-node")
     public void removeNode(@RequestParam("ipAddress") String ipAddress) {
-        namingServer.removeNode(ipAddress);
+        namingRepository.delete(ipAddress);
     }
 
     @GetMapping("/get-node")
-    public String getNode(@RequestParam("hashValue") int hashValue) {
-        return namingServer.getNode(hashValue);
-    }*/
+    public Optional<String> getNode(@RequestParam("hashValue") int hashValue) {
+        Optional<String> ip = namingRepository.findById(hashValue);
+        if(ip.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Hash not found!");
+        }
+        return ip;
+    }
 }
 
