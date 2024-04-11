58b5ddca313d44a7e917ce9d242fc35a0eb88c0f
rienvancampenhout <rien1van2campenhout3@gmail.com>
2024-04-11 19:44:35 +0200

    Previous commit did not include all the files and some files were incorrect

--- src/main/java/origin/project/ProjectApplication.java
+++ src/main/java/origin/project/ProjectApplication.java
@@ -2,18 +2,10 @@ package origin.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import origin.project.naming.server.NamingServer;
+
 
 @SpringBootApplication
-//@EnableEurekaServer
 public class ProjectApplication {
-    @Bean
-    public NamingServer namingServer() {
-        return new NamingServer();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
--- src/main/java/origin/project/naming/controller/NamingServerController.java
+++ src/main/java/origin/project/naming/controller/NamingServerController.java
@@ -1,31 +1,81 @@
 package origin.project.naming.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import origin.project.naming.repository.NamingEntry;
 import origin.project.naming.server.NamingServer;
 
-@RequestMapping("/naming-server")
+@Getter
+@Setter
+@RestController
+@RequestMapping("/namingServer")
 public class NamingServerController {
 
-    /*@Autowired
+    @Autowired
     private NamingServer namingServer;
 
-    @PostMapping("/add-node")
-    public void addNode(@RequestParam("ipAddress") String ipAddress) {
-        namingServer.addNode(ipAddress);
+    @PostMapping("/addNode")
+    public ResponseEntity<String> addNode(@RequestParam String nodeName, @RequestParam String ipAddress) {
+        try {
+            namingServer.addNode(nodeName, ipAddress);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Node '" + nodeName + "' added successfully");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
-    @PostMapping("/remove-node")
-    public void removeNode(@RequestParam("ipAddress") String ipAddress) {
-        namingServer.removeNode(ipAddress);
+    @DeleteMapping("/removeNode")
+    public ResponseEntity<String> removeNode(@RequestParam String ipAddress) {
+        try {
+            namingServer.removeNode(ipAddress);
+            return ResponseEntity.ok("Node '" + ipAddress + "' removed successfully");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
-    @GetMapping("/get-node")
-    public String getNode(@RequestParam("hashValue") int hashValue) {
-        return namingServer.getNode(hashValue);
-    }*/
+    // Endpoint to get node by ip-address
+    @GetMapping("/getNode")
+    public ResponseEntity<NamingEntry> getNodeByIp(@RequestParam String ipAddress) {
+        try {
+            NamingEntry node = namingServer.getNodeByIp(ipAddress);
+            return ResponseEntity.ok(node);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/getAllNodes")
+    public ResponseEntity<Iterable<NamingEntry>> getAllNodes() {
+        try {
+            return ResponseEntity.ok(namingServer.getAllNodes());
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/getFileLocation")
+    public ResponseEntity<String> getFileLocation(@RequestParam String fileName) {
+        try {
+            String location = namingServer.getFileLocation(fileName);
+            return ResponseEntity.ok("File '" + fileName + "' is located at node with IP: " + location);
+        } catch (IllegalStateException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/clearRepository")
+    public ResponseEntity<String> clearRepository() {
+        try {
+            namingServer.deleteAll();
+            return ResponseEntity.ok("NodeRepository cleared successfully");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
 
