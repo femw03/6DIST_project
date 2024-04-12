3c155e19d0dc567f228476d8dac2e8bf5ce7d755
Gones Anseel <gones.anseel@student.uantwerpen.be>
2024-04-12 16:14:36 +0200

    fix(naming): fix test-class declaration for building SpringBootApplication.

--- src/test/java/origin/project/ProjectApplicationTests.java
+++ src/test/java/origin/project/ProjectApplicationTests.java
@@ -2,8 +2,9 @@ package origin.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import origin.project.naming.ProjectApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = ProjectApplication.class)
 class ProjectApplicationTests {
 
     @Test
