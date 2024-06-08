74e51e49e33d049e726fa0bb9c6e94ebba02bf82
Gones Anseel <gones.anseel@student.uantwerpen.be>
2024-06-09 01:59:36 +0200

    feat(agents): Agents are started after replication is finished.

--- src/main/java/origin/project/client/service/ReplicationService.java
+++ src/main/java/origin/project/client/service/ReplicationService.java
@@ -2,6 +2,7 @@ package origin.project.client.service;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import jade.wrapper.StaleProxyException;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,14 +60,14 @@ public class ReplicationService {
             }
             try {
                 actualInit();
-            } catch (UnknownHostException e) {
+            } catch (UnknownHostException | StaleProxyException e) {
                 throw new RuntimeException(e);
             }
         }).start();
 
     }
 
-    public void actualInit() throws UnknownHostException{
+    public void actualInit() throws UnknownHostException, StaleProxyException {
         // allows for local testing
 //        System.out.println(node.getNamingServerIp());
 //        if (node.getNamingServerIp() == null) {
@@ -83,6 +84,7 @@ public class ReplicationService {
         System.out.println("replicated path: " + node.getREPLICATED_FILES_PATH());
         // verify local files and send hash-values to naming server
         startUp();
+        node.startAgents(currentLocalFiles);
 
         // start update thread
         updateThreadRunning = true;
